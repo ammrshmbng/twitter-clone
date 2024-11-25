951ba8e60b7a9a118a9036d76e121ad56af356ee
ammrshmbng <ammarmare01@gmail.com>
2024-11-25 13:47:02 +0700

    add user routes, user controller, and model notification

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.routes.js";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
+
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 app.use(express.json({ limit: "5mb" })); // to parse req.body
 // limit shouldn't be too high to prevent DOS
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
