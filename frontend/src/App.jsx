3aeab523ec3212d952bc36f5f052603316199d3f
ammrshmbng <ammarmare01@gmail.com>
2024-11-26 16:07:21 +0700

    add ProfilePage and EditProfileModal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import SignUpPage from './pages/auth/signup/SignUpPage';
 import Sidebar from './components/common/Sidebar';
 import RightPanel from './components/common/RightPanel';
 import NotificationPage from './pages/notification/NotificationPage';
+import ProfilePage from './pages/profile/ProfilePage';
 
 function App() {
     return (
@@ -15,6 +16,8 @@ function App() {
                 <Route path='/login' element={<LoginPage />} />
                 <Route path='/signup' element={<SignUpPage />} />
 				<Route path='/notifications' element={<NotificationPage />} />
+				<Route path='/profile/:username' element={<ProfilePage /> } />
+
 
             </Routes>
             <RightPanel/>
