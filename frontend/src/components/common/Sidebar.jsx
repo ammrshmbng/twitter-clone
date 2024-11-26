ec01b583e2576732644ba08f488202f944a4288a
ammrshmbng <ammarmare01@gmail.com>
2024-11-26 21:45:04 +0700

    add authUser in SideBar

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -5,7 +5,7 @@ import { IoNotifications } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 const Sidebar = () => {
@@ -34,11 +34,8 @@ const Sidebar = () => {
 		},
 	});
 
-	const data = {
-		fullName: "John Doe",
-		username: "johndoe",
-		profileImg: "/avatars/boy1.png",
-	};
+	const { data: authUser } = useQuery({ queryKey: ["authUser"] });
+
 
 	return (
 		<div className='md:flex-[2_2_0] w-18 max-w-52'>
@@ -68,7 +65,7 @@ const Sidebar = () => {
 
 					<li className='flex justify-center md:justify-start'>
 						<Link
-							to={`/profile/${data?.username}`}
+							to={`/profile/${authUser?.username}`}
 							className='flex items-center gap-3 py-2 pl-2 pr-4 transition-all duration-300 rounded-full cursor-pointer hover:bg-stone-900 max-w-fit'
 						>
 							<FaUser className='w-6 h-6' />
@@ -76,20 +73,20 @@ const Sidebar = () => {
 						</Link>
 					</li>
 				</ul>
-				{data && (
+				{authUser && (
 					<Link
-						to={`/profile/${data.username}`}
+						to={`/profile/${authUser.username}`}
 						className='mt-auto mb-10 flex gap-2 items-start transition-all duration-300 hover:bg-[#181818] py-2 px-4 rounded-full'
 					>
 						<div className='hidden avatar md:inline-flex'>
 							<div className='w-8 rounded-full'>
-								<img src={data?.profileImg || "/avatar-placeholder.png"} />
+								<img src={authUser?.profileImg || "/avatar-placeholder.png"} />
 							</div>
 						</div>
 						<div className='flex justify-between flex-1'>
 							<div className='hidden md:block'>
-								<p className='w-20 text-sm font-bold text-white truncate'>{data?.fullName}</p>
-								<p className='text-sm text-slate-500'>@{data?.username}</p>
+								<p className='w-20 text-sm font-bold text-white truncate'>{authUser?.fullName}</p>
+								<p className='text-sm text-slate-500'>@{authUser?.username}</p>
 							</div>
 							<BiLogOut
 								className='w-5 h-5 cursor-pointer'
