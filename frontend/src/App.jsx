8edb6e8cd91aeeb9d8784b1456fa650a14137814
ammrshmbng <ammarmare01@gmail.com>
2024-11-26 12:43:26 +0700

    create signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/home/HomePage';
+import LoginPage from './pages/auth/login/LoginPage';
+import SignUpPage from './pages/auth/signup/SignUpPage';
 
-const App = () => {
-  return (
-	<div className="text-3xl ">App</div>
-  )
+function App() {
+    return (
+        <div className='flex max-w-6xl mx-auto'>
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/login' element={<LoginPage />} />
+                <Route path='/signup' element={<SignUpPage />} />
+            </Routes>
+        </div>
+    );
 }
 
-export default App
+export default App;
