930eb4b428735e1cfa425d7e8c6ef27a38b0f9df
ammar sihombing <62658874+ammrshmbng@users.noreply.github.com>
2024-11-29 10:09:54 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	// Tambahkan konfigurasi build ini
+	build: {
+		outDir: "dist"
+	},
 	server: {
 		port: 3000,
 		proxy: {
